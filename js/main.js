fa716edc888a413a27d4ba9d0e7a170a6d859aa4
Michael Frampton <mframp@gmail.com>
2017-06-07 19:58:08 -0700

    aura no pics

--- js/main.js
+++ js/main.js
@@ -142,7 +142,24 @@ $(document).ready(function() {
           $('#SubNav').removeClass('navbar-fixed-top');
         }
     }
-  });  
+  }); 
+
+
+// https://www.w3schools.com/w3css/w3css_slideshow.asp
+  var slideIndex = 0;
+  carousel();
+
+  function carousel() {
+      var i;
+      var x = document.getElementsByClassName("story");
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+      }
+      slideIndex++;
+      if (slideIndex > x.length) {slideIndex = 1} 
+      x[slideIndex-1].style.display = "block"; 
+      setTimeout(carousel, 2500); // Change image every 2 seconds
+  } 
 
                  
   });
